a72eabe9311a3695b640da7419120e4cdeea05be
Alejandra Marin <Alosada2003@gmail.com>
2025-05-16 15:56:57 -0500

    v.03

--- docs/programa.js
+++ docs/programa.js
@@ -6,3 +6,24 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 }).addTo(map);
 
 var marker = L.marker([51.5, -0.09]).addTo(map);
+marker.addTo(map);
+
+async function Cargarpuntos(){
+    var miArchivo= await fetch("microondas.geojson");
+
+    var datos= await miArchivo.json();
+
+    let listaFeatures= datos ["features"];
+
+    for(int i=0; i<10; 1++){
+        console.log(datos["features"][i]);
+
+        listaFeatures[i]["geometry"]["coordiantes"];
+        var miMarcador = L.marker(misCoordenadas);
+        marker.addTo(map);
+    }
+    Cargarpuntos();
+
+  
+    
+}
