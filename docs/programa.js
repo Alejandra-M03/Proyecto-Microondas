a2012a26df222a67552568a9abbde407c7ee29be
Alejandra-M03 <alosada2003@gmail.com>
2025-05-19 14:25:06 -0500

    V.01 programa.js

--- docs/programa.js
+++ docs/programa.js
@@ -7,23 +7,25 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 
 var marker = L.marker([51.5, -0.09]).addTo(map);
 marker.addTo(map);
-
+//Abrir el archivo de GeoJson
+//Javasscript se ejecuta de tal manera que si una linea de codigo se demora, se pasa a la siguiente sin esperar
 async function Cargarpuntos(){
     var miArchivo= await fetch("microondas.geojson");
 
+    //Convertir el archivo a Json: objeto js:
     var datos= await miArchivo.json();
-
+    //Obtener el arreglo de la llavr features
     let listaFeatures= datos ["features"];
 
     for(int i=0; i<10; 1++){
         console.log(datos["features"][i]);
-
+    //Obtener la geometria del primer elemento 
         listaFeatures[i]["geometry"]["coordiantes"];
         var miMarcador = L.marker(misCoordenadas);
         marker.addTo(map);
     }
     Cargarpuntos();
 
-  
-    
+    const marker = L.marker([4.60971, -74.08175]).addTo(map);
+    marker.bindPopup('<strong>Marcador 1</strong><br>Información adicional del marcador.');  
 }
